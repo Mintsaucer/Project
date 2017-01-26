7a316772a610bfc112fe730c20e4591e1271c87e
Mintsaucer <koskinen.late@gmail.com>
2017-01-26 20:04:10 +0200

    Add files via upload

---
+++ bluetooth.ino
@@ -0,0 +1,45 @@
+#include <DHT11.h>
+
+//#include <SoftwareSerial.h>   //Serial library
+
+int pin = 4;
+DHT11 dht11(pin);   
+
+// connect Arduino  TX pin to dongle RX pin 
+// SoftwareSerial bt (5,6); // RX, TX
+
+int btdata;   // sensordata bluetooth
+
+void setup() {
+  //bt.begin (9600);
+  Serial.begin (9600);
+}
+  void loop() {
+    int error;
+    float temp, humi;
+    if ((error = dht11.read(humi, temp)) == 0)
+    {
+    Serial.print("temperature:");
+    Serial.print(temp);
+    Serial.print(" humidity:");
+    Serial.print(humi);
+    Serial.println();
+  }
+  else
+  {
+    Serial.println();
+    Serial.print("Error No");
+    Serial.print(error);
+    Serial.println();    
+  }
+  delay(DHT11_RETRY_DELAY); //delay for reread
+
+    
+
+  // Send data to serial bluetooth
+  // bt.print(String(btdata) + "test");
+  // bt.print("\n");
+  
+  //  delay(2000);
+  }
+
