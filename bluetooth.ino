43406e60f5eaa9412c981497684609ab45858105
Mintsaucer <koskinen.late@gmail.com>
2017-01-27 11:00:03 +0200

    Add files via upload

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,45 +1,50 @@
-#include <DHT11.h>
+// Arduino Uno 101 sending sensordata over serial bluetooth to Raspberry Pi 3
 
-//#include <SoftwareSerial.h>   //Serial library
+#include <DHT11.h> // Sensor library
+#include <SoftwareSerial.h>   // Serial library
 
-int pin = 4;
+int pin = 4;    //digital pin
 DHT11 dht11(pin);   
 
 // connect Arduino  TX pin to dongle RX pin 
-// SoftwareSerial bt (5,6); // RX, TX
+SoftwareSerial bt (5,6); // RX, TX
 
-int btdata;   // sensordata bluetooth
+//int btdata;   // sensordata bluetooth
 
-void setup() {
-  //bt.begin (9600);
+void setup() 
+{
+  bt.begin (9600);
   Serial.begin (9600);
 }
-  void loop() {
-    int error;
-    float temp, humi;
-    if ((error = dht11.read(humi, temp)) == 0)
-    {
-    Serial.print("temperature:");
-    Serial.print(temp);
-    Serial.print(" humidity:");
-    Serial.print(humi);
-    Serial.println();
+void loop() 
+{
+  int error;
+  float temp, humi;
+  if ((error = dht11.read(humi, temp)) == 0)
+  {
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" Humidity: ");
+  Serial.print(humi);
+  Serial.print("%");
+  Serial.println();
   }
   else
   {
-    Serial.println();
-    Serial.print("Error No");
-    Serial.print(error);
-    Serial.println();    
+  Serial.println();
+  Serial.print("Error No. ");
+  Serial.print(error);
+  Serial.println();    
   }
   delay(DHT11_RETRY_DELAY); //delay for reread
 
     
-
   // Send data to serial bluetooth
-  // bt.print(String(btdata) + "test");
+  // bt.print(String(temp) + "," + String(humi));
+  
+  // bt.print(String(humi) + "humidity");
   // bt.print("\n");
   
   //  delay(2000);
-  }
+}
 
